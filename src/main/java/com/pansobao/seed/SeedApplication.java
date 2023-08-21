6e970ad586a5ec9029010442e47bab9bfe276457
memeo <proyectosjgot@gmail.com>
2023-08-21 11:06:53 -0400

    update package

--- src/main/java/com/pansobao/seed/SeedApplication.java
+++ src/main/java/com/pansobao/seed/SeedApplication.java
@@ -1,4 +1,4 @@
-package com.pansobao.seed.seed;
+package com.pansobao.seed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/pansobao/seed/SeedApplicationTests.java
+++ src/test/java/com/pansobao/seed/SeedApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pansobao.seed.seed;
+package com.pansobao.seed;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
